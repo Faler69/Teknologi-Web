fccd8d24ec62a006797e7e26f4ac8a05c49fddc5
Muhammad Farel <lordfarel69@gmail.com>
2025-07-10 18:37:08 +0700

    Update hapusmhs.php

--- hapusmhs.php
+++ hapusmhs.php
@@ -6,13 +6,13 @@ if(hapusmhs($id) > 0) {
     echo "
     <script>
         alert('Data Mahasiswa Berhasil Dihapus!');
-        window.location.href = '../datamhs.php';
+        window.location.href = 'datamhs.php';
     </script>";
 } else {
     echo "
     <script>
         alert('Data Mahasiswa Gagal Dihapus!');
-        window.location.href = '../datamhs.php';
+        window.location.href = 'datamhs.php';
     </script>";
 }
 
